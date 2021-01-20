4a6ff4e35ce45e4f81a650f608f7020df7f1ff37
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-21 00:03:31 +0530

    :sparkles: Add Google OAuth

--- src/api/User/router.ts
+++ src/api/User/router.ts
@@ -1,13 +1,26 @@
 import { Request, Response, Router } from 'express';
+import LoggerInstance from '../../loaders/logger';
+import { generateLoginUrl, setToken } from './controller';
 
 const router = Router();
 
 export const userRouteHandler = () => {
   router.get('/login', loginHandler);
+  router.get('/authcode', authCodeHandler);
 
   return router;
 };
 
 const loginHandler = (req: Request, res: Response) => {
-  res.send('Login URL');
+  res.redirect(generateLoginUrl());
+};
+
+const authCodeHandler = async (req: Request, res: Response) => {
+  try {
+    let data = await setToken(req.query);
+    res.send(`Hi ${data.name}! Welcome to Canvas`);
+  } catch (error) {
+    LoggerInstance.error(error);
+    res.json({ status: 500, message: error.message });
+  }
 };
