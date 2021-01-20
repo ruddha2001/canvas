0574ace106835027166541844d1d6db37a3ad06e
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-20 21:36:30 +0530

    :adhesive_bandage: Fix route handler

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,7 @@ import { userRouteHandler } from './User/router';
 export default (): Router => {
   const app = Router();
 
-  app.use('/user', userRouteHandler);
+  app.use('/user', userRouteHandler());
 
   return app;
 };
