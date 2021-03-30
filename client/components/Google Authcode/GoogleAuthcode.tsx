9b5620033c2bfd5401327df275491e4bd40b393b
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-03-31 02:08:03 +0530

    :passport_control: Add auth workflow

--- client/components/Google Authcode/GoogleAuthcode.tsx
+++ client/components/Google Authcode/GoogleAuthcode.tsx
@@ -1,19 +1,42 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { BASE_URL } from '../../constants';
 
 export default function GoogleAuthcode() {
   const [code, setCode] = useState('');
   const [message, setMessage] = useState('Please wait while we log you in...');
+  const [help, setHelp] = useState('');
   useEffect(() => {
     const queryObject = new URLSearchParams(window.location.search);
     setCode(queryObject.get('code'));
-    console.log(code);
+    if (queryObject.get('error')) {
+      setMessage('There was some problem authenticating with Google');
+      return setHelp('Please retry or contact me@aniruddha.net for further help');
+    }
+    axios
+      .get(`${BASE_URL}/api/user/authcode?code=${code}`, {
+        validateStatus: function (status) {
+          return status >= 200 && status < 500;
+        },
+      })
+      .then(response => {
+        if (response.status != 200) {
+          setMessage('We could not authenticate your account');
+          return setHelp('Please retry or contact me@aniruddha.net for further help');
+        }
+        window.localStorage.setItem('token', response.data.token);
+        window.localStorage.setItem('auth', 'true');
+        window.location.replace('/dashboard');
+      });
   });
 
   return (
     <>
-      <p className="mx-auto mt-72 w-max font-bold text-2xl">
+      <p className="mx-auto mt-72 w-max text-center">
         <img src="/canvas.png" className="w-40 mx-auto pb-10" />
-        {message}
+        <span className="font-bold text-2xl">{message}</span>
+        <br />
+        {help}
       </p>
     </>
   );
--- client/components/Sign In/SignInButton.tsx
+++ client/components/Sign In/SignInButton.tsx
@@ -1,8 +1,10 @@
+import { BASE_URL } from '../../constants';
+
 export default function SignInButton() {
   return (
     <p className={['inline'].join(' ')}>
       Login and take a step towards self-analysis
-      <a href="/api/user/login">
+      <a href={`${BASE_URL}/api/user/login`}>
         <img className={['block', 'w-52', 'md:w-60', 'self-center', 'm-auto'].join(' ')} src="/google_btn.png" />
       </a>
     </p>
