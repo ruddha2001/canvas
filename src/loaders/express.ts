a0ed0817e362b30a3c815fa08975e84e7feb65ee
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-03-31 02:07:42 +0530

    :heavy_minus_sign: Remove body-parser

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,7 +1,5 @@
-import bodyParser from 'body-parser';
 import cors from 'cors';
-import { join } from 'path';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import helmet from 'helmet';
 import config from '../config';
 import routes from '../api';
@@ -38,19 +36,8 @@ export default ({ app }: { app: express.Application }): void => {
   app.use(cors());
 
   // Middleware that transforms the raw string of req.body into json
-  app.use(bodyParser.json());
-
-  // Serve the client
-  // app.use(express.static(join(__dirname, '..', '..', 'build', 'client')));
-  // app.get('/', (req: Request, res: Response) => {
-  //   res.sendFile(join(__dirname, '..', '..', 'build', 'client', 'index.html'));
-  // });
+  app.use(express.json());
 
   // Load API routes
   app.use(config.api.prefix, routes());
-
-  // Redirect to index.html for routing
-  // app.get('*', (req: Request, res: Response) => {
-  //   res.sendFile(join(__dirname, '..', '..', 'build', 'client', 'index.html'));
-  // });
 };
