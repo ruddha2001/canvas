fa2050f329b8a4fc2f6b01d64bbfa4b37897c5f9
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-04-14 03:41:07 +0530

    :sparkles: Complete feedback page

---
+++ client/components/Feedback/Feedback.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Feedback() {
+  const [feedbackRow, setFeedbackRow] = useState(15);
+
+  useEffect(() => {
+    const width = window.innerHeight;
+    if (width < 900) setFeedbackRow(10);
+    else if (width < 730) setFeedbackRow(8);
+  });
+  return (
+    <div className="pt-8">
+      <p className="text-3xl md:text-5xl pb-10">Let us know how we have been doing</p>
+      <p className="text-2xl pb-5">Feedback</p>
+      <textarea
+        className="border border-black w-full rounded-3xl resize-none p-2"
+        rows={feedbackRow}
+        name="feedback-text"
+        id="feedback-text"
+      ></textarea>
+      <p className="text-2xl py-5">Do you want us to contact you?</p>
+      <select className="border border-black rounded-3xl resize-none p-2 bg-white text-xl px-6">
+        <option value="no">No</option>
+        <option value="yes">Yes</option>
+      </select>
+      <p className="pt-5 w-full text-2xl md:text-3xl text-center">
+        <button className="bg-base-primary text-white rounded-full px-6 py-3">Submit</button>
+      </p>
+    </div>
+  );
+}
--- client/pages/feedback/index.tsx
+++ client/pages/feedback/index.tsx
@@ -1,29 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Loader from 'react-loader';
+import Feedback from '../../components/Feedback/Feedback';
 import Footer from '../../components/Footer/Footer';
 import MenuBar from '../../components/Menu Bar/MenuBar';
+import { useAuthenticate } from '../../hooks/AuthenticateHook';
 
 export default function index() {
-  const [loaded, setLoaded] = useState(true);
-  // useEffect(() => {
-  //   if (window.localStorage.getItem('auth') !== 'true') {
-  //     alert('Please login to continue.');
-  //     return window.location.replace('/');
-  //   }
-  //   useAuthenticate(window.localStorage.getItem('token'))
-  //     .then(_ => {
-  //       setLoaded(true);
-  //     })
-  //     .catch(_ => {
-  //       alert('There was problem verifying your identify. Please re-login to continue.');
-  //       return window.location.replace('/');
-  //     });
-  // });
+  const [loaded, setLoaded] = useState(false);
+  const [name, setName] = useState('');
+  useEffect(() => {
+    if (window.localStorage.getItem('auth') !== 'true') {
+      alert('Please login to continue.');
+      return window.location.replace('/');
+    }
+    useAuthenticate(window.localStorage.getItem('token'))
+      .then(_ => {
+        setLoaded(true);
+        setName(window.localStorage.getItem('name'));
+      })
+      .catch(_ => {
+        alert('There was problem verifying your identify. Please re-login to continue.');
+        return window.location.replace('/');
+      });
+  });
   return (
     <Loader loaded={loaded}>
-      <MenuBar name="Ruddha" />
+      <MenuBar name={name} />
       <div></div>
-      <div className="px-3 md:px-10 min-h-full">Feedback</div>
+      <div className="px-3 md:px-10 min-h-full">
+        <Feedback />
+      </div>
       <Footer />
     </Loader>
   );
