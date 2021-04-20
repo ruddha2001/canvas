a82e3f6a39aa096427927e77d3fa2ef86d66d4ff
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-04-20 13:57:43 +0530

    :sparkles: Add hyperlink to generate report button

--- client/components/Generate Report Button/GenerateReportButton.tsx
+++ client/components/Generate Report Button/GenerateReportButton.tsx
@@ -5,7 +5,14 @@ export default function GenerateReportButton() {
     <div className="w-full mt-10 md:mt-5 lg:mt-0">
       <p className="w-full text-center sm:pt-4 md:pt-2 lg:pt-9 lg:text-2xl">
         You can generate a more detailed report{' '}
-        <button className="bg-base-primary text-white rounded-2xl p-2">Generate Report</button>
+        <button
+          className="bg-base-primary text-white rounded-2xl p-2"
+          onClick={() => {
+            window.location.href = '/generate';
+          }}
+        >
+          Generate Report
+        </button>
       </p>
     </div>
   );
