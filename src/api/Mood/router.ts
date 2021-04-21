e6734fec8bbac6175b8d36e8989e8a5d884cc29c
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-04-21 21:21:58 +0530

    :sparkles: Add middleware

--- src/api/Mood/router.ts
+++ src/api/Mood/router.ts
@@ -1,19 +1,20 @@
 import { Request, Response, Router } from 'express';
 import LoggerInstance from '../../loaders/logger';
+import { userAuthMiddleware } from '../Shared/middlewares/userAuthMiddleware';
 import { addNewMood, getUserMoods } from './controller';
 
 const router = Router();
 
 export const moodRouteHandler = () => {
-  router.post('/add', addMood);
-  router.get('/fetch', fetchMood);
+  router.post('/add', userAuthMiddleware, addMood);
+  router.get('/fetch', userAuthMiddleware, fetchMood);
 
   return router;
 };
 
 const addMood = async (req: Request, res: Response) => {
   try {
-    await addNewMood(req.body.mood, /**res.locals.user.email*/ 'ruddha.mine@gmail.com');
+    await addNewMood(req.body.mood, res.locals.user.email);
     res.json({ success: true, message: 'New mood has been added.' });
   } catch (error) {
     LoggerInstance.error(error);
@@ -23,7 +24,7 @@ const addMood = async (req: Request, res: Response) => {
 
 const fetchMood = async (req: Request, res: Response) => {
   try {
-    res.json(await getUserMoods(/**res.locals.user.email*/ 'ruddha.mine@gmail.com', req.query.full as string));
+    res.json(await getUserMoods(res.locals.user.email, req.query.full as string));
   } catch (error) {
     LoggerInstance.error(error);
     res.status(500).json({ success: false, message: error.message });
