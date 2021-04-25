fd829764f116f9716eec2ba348f8f02ec720071d
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-04-25 21:19:30 +0530

    :white_check_mark: Add test cases

---
+++ src/test/mood.test.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import 'mocha';
+import { addNewMood, getUserMoods } from '../api/Mood/controller';
+
+/**
+ * Takes in a function and checks for error
+ * @param {Function} method - The function to check
+ * @param {any[]} params - The array of function parameters
+ * @param {string} message - Optional message to match with error message
+ */
+const expectThrowsAsync = async (method: Function, params: any[], message?: string) => {
+  let err = null;
+  try {
+    await method(...params);
+  } catch (error) {
+    err = error;
+  }
+  if (message) {
+    expect(err.message).to.be.equal(message);
+  } else {
+    expect(err).to.be.an('Error');
+  }
+};
+
+describe('Mood controller', () => {
+  it('Add an invalid new mood', async () => {
+    await expectThrowsAsync(addNewMood, ['elated', '', 'sample@mail.com']);
+  });
+  it('Fetch moods for non-existent user', async () => {
+    await expectThrowsAsync(getUserMoods, ['sample@mail.com']);
+  });
+});
